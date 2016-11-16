4c281afb04eaad9d86dbcad45b582926f045d170
altexo <altexo94@gmail.com>
2016-11-16 12:01:36 -0700

    Funciones de ajax para desplegar datos de json en el cuerpo del html

--- JQueryPractica/js/eventos.js
+++ JQueryPractica/js/eventos.js
@@ -1,10 +1,20 @@
 // DOM = Modelo de objetos del documento
+var i = 0;
 var inicio = function()
 {
 
 	var dameclic = function()
 	{
-		alert("Dame màs >o<");
+
+		$.ajax({
+  			url: 'https://randomuser.me/api/',
+  			dataType: 'json',
+  			success: function(data) {
+  				$("#txtNombre").val(data.results[0].name.first+ " " +data.results[0].name.last);
+  				$("#imgFoto").attr("src", data.results[0].picture.medium);
+   			console.log(data.results[0].name.first+ " " +data.results[0].name.last);
+  			}
+		});
 	}
 	$("#dameClic").on("click",dameclic);
 }
