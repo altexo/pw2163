faad8577d18dbccd4722b398b255b7118e49fb12
altexo <altexo94@gmail.com>
2016-12-05 11:52:25 -0700

    Utileria php y eventos nuevos

--- usuarios/js/eventos.js
+++ usuarios/js/eventos.js
@@ -1,42 +1,74 @@
 var inicioUsuarios = function()
 {
+	
 	var validaUsuario = function()
 	{
+
+		//Extraer los datos de los input en el HTML
 		var usuario = $("#txtUsuario").val();
-		var clave = $("#txtClave").val();
-		//preparar los parametros para AJAX
-		var parametros = 	"opcion=valida"+
-							"&usuario="+usuario+
-							"&clave="+clave+
-							"&id="+Math.random();
-		//Hacemos la peticion remota
-		$.AJAX({
-			cache:false,
-			type:"POST",
-			dataType: "JSON",
-			url: "php/utilerias.php",
-			data:parametros,
-			success: function(response)
-			{
-
-			},
-			error: function(xhr, ajaxOptions, thrownError)
-			{
-
-			}
-		});
-
-		//Validadmos que no esten vacios
-		if (usuario != "" && clave != "") 
+		var clave   = $("#txtClave").val();
+		//Preparar los parámetros para AJAX
+		var parametros = "opcion=valida"+
+		                 "&usuario="+usuario+
+		                 "&clave="+clave+
+		                 "&id="+Math.random();
+		
+		//Validamos que no esten vacíos
+		if(usuario!="" && clave!="")
 		{
-
-		}else
+			//Hacemos la petición remota
+			$.ajax({
+				cache:false,
+				type:"POST",
+				dataType:"json",
+				url: "php/utilerias.php",
+				data:parametros,
+				success: function(response){
+					if(response.respuesta == true)
+					{    
+						$("#entradaUsuario").hide("slow");
+						$("nav").show("slow"); 
+					}
+					else
+					{
+						alert("Datos incorrectos :(");
+					}
+				},
+				error: function(xhr,ajaxOptions,thrownError){
+					//Si todo sale mal
+				}
+			});
+		}
+		else
 		{
-
+			alert("Usuario y clave son obligatorios");
 		}
 	}
-	$("#btnValidUsuario").on("click", validaUsuario);
-}
+	$("#btnValidaUsuario").on("click",validaUsuario);
+	var teclaClave = function(tecla)
+	{
+		if(tecla.which == 13) //Tecla enter.
+		{
+			validaUsuario(); //Función que valida al usuario.
+		}	
+	}
 
+	var Alta = function()
+	{
+		$("#artAltaUsuarios").show("slow");
+	}
+
+	var GuardaUsuario = function()
+	{
+		
+	}
+
+	//keypress: se ejecuta cada vez que presiono una 
+	//tecla sobre el input.
+	$("#txtClave").on("keypress",teclaClave);
+	$("#btnAlta").on("click",Alta);
+	$("#btnGuardaUsuario").on("click",GuardaUsuario);
+}
 //Evento inicial
-$(document).on("ready", inicioUsuarios);
+$(document).on("ready",inicioUsuarios);
+
--- usuarios/php/utileria.php
+++ usuarios/php/utileria.php
@@ -1,48 +1,73 @@
 <?php
-
+	//Limpiar parámetros - contra ataques
 	function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
 	{
-		  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-
-		  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
-
-		  switch ($theType) {
-		    case "text":
-		      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-		      break;    
-		    case "long":
-		    case "int":
-		      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-		      break;
-		    case "double":
-		      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
-		      break;
-		    case "date":
-		      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-		      break;
-		    case "defined":
-		      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-		      break;
-		  }
-		  return $theValue;
+	  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+	  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+	  switch ($theType) {
+	    case "text":
+	      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+	      break;    
+	    case "long":
+	    case "int":
+	      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+	      break;
+	    case "double":
+	      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+	      break;
+	    case "date":
+	      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+	      break;
+	    case "defined":
+	      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+	      break;
+	  }
+	  return $theValue;
 	}
 	function validaUsuario()
-	{
-		$u = GetSQLValueString($_POST["usuario"], "text");
-		$c = $_POST["clave"];
-		$conexion = mysql_connect("localhost","root","");
-		$consulta = sprintf("select * from usuarios where Usuario=%s and clave=%s")
+	{		
+		$respuesta = false;			
+		$u = GetSQLValueString($_POST["usuario"],"text"); //limpieza
+		$c = GetSQLValueString($_POST["clave"],"text"); //limpieza
+		//Conexión al servidor
+		$conexion  = mysql_connect("localhost","root","");
+		//Conexión a la base de datos
+		mysql_select_db("bd2163");
+		$consulta  = sprintf("select usuario,clave from usuarios where usuario=%s and clave=%s limit 1",$u,$c);
+		$resultado = mysql_query($consulta);
+		//Esperamos un solo resultado
+		if(mysql_num_rows($resultado) == 1)
+		{
+			$respuesta = true;
+		}
+		$arregloJSON = array('respuesta' => $respuesta );
+		print json_encode($arregloJSON);
 	}
+	function buscaUsuario()
+     {
+     	$respuesta = false;
+     	$u = GetSQLValueString($_POST["usuario"], "text");
+     	$conexion = mysql_connect("localhost","root","");
+     	mysql_select_db("bd2163")-,
+     	$consulta = sprintf("SELECT * FROM usuarios WHERE usuario=%s", $u);
+     	$resultado = mysql_query($consulta);
+     	if (mysql_num_rows($resultado) > 0) {
+     		$respuesta = true;
+     		if ($registro = mysql_fetch_array($resultado)) {
+     			# code...
+     		}
+     	}
+     }
+	//Menú principal
 	$opc = $_POST["opcion"];
 	switch ($opc) {
 		case 'valida':
 			validaUsuario();
 			break;
 		
-		default:
-			
+		default: 'buscaUsuario':
+			buscaUsuario();
+			# code...
 			break;
 	}
-
-
 ?>
